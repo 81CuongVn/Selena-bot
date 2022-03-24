7b65e6350cb5edd20442bda42b56fcc5942baeba
Teófilo Nogueira <53917980+TeoNogueira@users.noreply.github.com>
2022-03-24 20:32:12 +0000

    Module.exports = and JSON.stringify()

---
+++ chatbot.js
@@ -0,0 +1,25 @@
+const otherList = [{
+name: 'Teonogueira',
+  Idade: 31,
+  Person: 'Selena',
+  Clan: 'WindGarden',
+  Rank: 'Mythic',
+
+},
+ {                  
+  name: `Rick`,
+  Idade: 35,
+  Person: 'Kagura',
+  Clan: 'WindGarden',
+  Rank: 'Mythic',
+
+}]
+
+
+
+module.exports = JSON.stringify(otherList)
+
+
+// /
+///
+///
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const Discord = require("discord.js");
 const { Intents } = require("discord.js");
 const dotenv = require("dotenv");
+const chatBot = require('./chatbot.js');
+
+// https://discord.js.org/#/docs/discord.js/stable/general/welcome
+//
 
 dotenv.config();
 
 const client = new Discord.Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
+
 const listCommands = `Membros superiores:\n
 Teófilo Nogueira,\n
-Bot Selena,\n`
+Bot Selena.\n
+Para mais detalhes entre em contato com : cyberstileid397@gmail.com`
 
-
-const prefix = "+";
+//PREFIX
+const pre = "!";
+//////
 
 client.login(process.env.TOKEN);
 
@@ -22,23 +29,30 @@ client.on("ready", () => {
 
 client.on("messageCreate", (msg) => {
   if (!msg.guild) return;
-  if (!msg.content.startsWith(prefix)) return;
+  if (!msg.content.startsWith(pre)) return;
 
-  if (msg.content === prefix + "Hello") {
+  if (msg.content === `${pre}Hello`) {
     msg.reply({
       content: "Is Okay!!",
     });
   }
 
-  if (msg.content === prefix + "Bom dia") {
+  if (msg.content === `${pre}Bom dia`) {
     msg.reply({
       content: "Bom dia, " + msg.author.username,
     });
   }
 
-  if(msg.content === prefix + "secretList") {
+  if(msg.content === `${pre}secretList` ) {
     msg.reply({
       content: `${listCommands}`,
     });   
   }//
+
+    if(msg.content === `${pre}adminList`) {
+    msg.reply({
+      content: `${chatBot}`,
+    });   
+  }
+
 });
